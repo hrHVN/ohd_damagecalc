d829ab66ab1ee64a5240daa17c3ced20b6809d00
Andreas <87428001+hrHVN@users.noreply.github.com>
2024-01-14 19:06:32 +0100

    simplified the code.
    Refactored the nested if-tree to be smarter and easier to read. This way we also do not rewrite the code 8 times. 
    
    - Using an Object to store the values for easier manipulation and access, and a for-loop to push the values to the page.
    - Since the list will be updated regardless, the "<ul>" element can be deleted each time.  
    - Added an alert box to tell the user why it doesn't work to use letters.

--- main.js
+++ main.js
@@ -1,85 +1,41 @@
 $(document).ready(function(){
     $("#btn").click(function(){
-        let n = 0;
         var value = $("#value").val();
-        if(!isNaN(value)){
-            if($("li").length){
-                var elements = $("li").toArray();
-                elements.forEach(element => {
-                    element.remove();
-                });
-                n = 0;
-            };
-            if(value*3 > 100){
-                $("#list").append("<li>Head: " + (value * 3).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Head: " + (value * 3).toFixed(2) + "</li>");
-            }
 
-            if(value*1.0 > 100){
-                $("#list").append("<li>Body: " + (value * 1.00).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Body: " + (value * 1.00).toFixed(2) + "</li>");
-            }
-
-            if(value*0.5 > 100){
-                $("#list").append("<li>Shoulders: " + (value * 0.5).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Shoulders: " + (value * 0.5).toFixed(2) + "</li>");
-            }
-
-            if(value*0.5 > 100){
-                $("#list").append("<li>Arms: " + (value * 0.5).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Arms: " + (value * 0.5).toFixed(2) + "</li>");
-            }
-
-            if(value*0.25 > 100){
-                $("#list").append("<li>Hands: " + (value * 0.25).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Hands: " + (value * 0.25).toFixed(2) + "</li>");
-            }
-
-            if(value*0.65 > 100){
-                $("#list").append("<li>Head: " + (value * 0.65).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Upper Legs: " + (value * 0.65).toFixed(2) + "</li>");
-            }
-            if(value*0.5 > 100){
-                $("#list").append("<li>Lower Legs: " + (value * 0.5).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Lower Legs: " + (value * 0.5).toFixed(2) + "</li>");
-            }
-            if(value*0.25 > 100){
-                $("#list").append("<li>Toes: " + (value * 0.25).toFixed(2) + "</li>");
-                $("li").css("background", "yellow");
-                $("li").css("color", "black");
-                n++;
-            } else{
-                $("#list").append("<li>Toes: " + (value * 0.25).toFixed(2) + "</li>");
-            }
-            
+        // Chechk if the input value is a valid number, if not show an error, and reload the window!
+        if(isNaN(value)){
+            window.alert("Pleas enter a valid type of number!");
+            location.reload();
         };
+            // Delete the list, before printing a new one!
+            $("li").text(" ");
+            
+            // Calculate all the Crits
+            let crit3x = (value * 3).toFixed(2);
+            let crit1x = (value * 1).toFixed(2);
+            let crit0_65x = (value * .65).toFixed(2);
+            let crit0_5x = (value * .5).toFixed(2);
+            let crit0_25x = (value * .25).toFixed(2);
+
+        // assign values to the elements
+        const bodyParts = {
+                head : {text:`Head: ${crit3x}`, crit: crit3x},
+                body :{text: `Body: ${crit1x}`, crit: crit1x},
+                shoulders:{text: `Shoulders: ${crit0_5x}`, crit: crit0_5x},
+                arms : `Arms: ${crit0_5x}` ,
+                hands : { text: `Hands: ${crit0_25x}`, crit: crit0_25 } ,
+                upper_legs : { text: `Upper Legs: ${crit0_65x}`, crit: crit0_65 },
+                lower_legs: { text: `Lower Legs: ${crit0_5x}`, crit: crit0_5x},
+                toes: { text: `Toes: ${crit0_25x}`, crit: crit0_25},
+            };
+        
+        // Append list elements to the page, and add corresponding css based on damage values!
+        for( let part in bodyParts){
+            let li = $('<li>').text(bodyParts[part].text);
+            if (bodyParts[part].crit >= 100) {
+                li.css('background-color', 'yellow').css('color', 'black');
+            }
+            $('#list').append(li);
+        }         
     });
 });
